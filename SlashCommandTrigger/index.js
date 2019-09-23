f2c301366ef37dc2d29d1b5126acccd714654591
Tuttle, Thomas <Thomas.Tuttle@advent.com>
2019-09-23 15:15:40 -0400

    Diagnostic slash command

--- SlashCommandTrigger/index.js
+++ SlashCommandTrigger/index.js
@@ -1,22 +1,16 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: {
-                "response_type": "ephemeral",
-                "text": "Vomming...",
-                "attachments": [
-                    { "text": JSON.stringify(req.body) }
-                ]
-            }
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    context.res = {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: {
+            'response_type': 'ephemeral',
+            'text': 'Voming...',
+            'attachments': [
+                { 'text': JSON.stringify(req.body) }
+            ]
+        }
+    };
 };
