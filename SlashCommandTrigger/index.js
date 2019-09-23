953b235a220090568bdf1f0e981c277ca9fc530f
Tuttle, Thomas <Thomas.Tuttle@advent.com>
2019-09-23 14:25:22 -0400

    Hello world

---
+++ SlashCommandTrigger/index.js
@@ -0,0 +1,22 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: {
+                "response_type": "ephemeral",
+                "text": "Vomming...",
+                "attachments": [
+                    { "text": JSON.stringify(req.body) }
+                ]
+            }
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+};
